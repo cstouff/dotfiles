e88a1d64796e3eec9ab17762407d3aa6c6b8a6a4
Chris Stouff <stouff@gmail.com>
2020-09-20 22:17:15 -0500

    Added hyper.lua and moveToScreen.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,14 @@
+-- Local Definitions
+local topScreen = "U28E590"
+local bottomScreen = "LG HDR WFHD"
+
+-- Hyper Configuration
+local hyper = require('hyper')
+hyper.install('F20') 
+
+local topScreen = "U28E590"
+local bottomScreen = "LG HDR WFHD"
+
 hs.window.animationDuration = 0
 units = {
   left50	= { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
@@ -16,8 +27,17 @@ units = {
   center	= { x = 0.25, y = 0.00, w = 0.50, h = 1.00 }
 }
 
+-- Hyper Key Binds
+hyper.bindKey('r', hs.reload)
+
+hyper.bindKey('1', function() hs.window.focusedWindow():moveToScreen(topScreen) end)
+hyper.bindKey('2', function() hs.window.focusedWindow():moveToScreen(bottomScreen) end)
+
 mash = { 'shift', 'ctrl', 'cmd' }
 hs.hotkey.bind(mash, 'o', function() hs.window.focusedWindow():move(units.left50,    nil, true) end)
 hs.hotkey.bind(mash, 'p', function() hs.window.focusedWindow():move(units.right50,    nil, true) end)
 hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
 hs.hotkey.bind(mash, 'c', function() hs.window.focusedWindow():move(units.center, nil, true) end)
+
+mash2 = { 'shift', 'ctrl' }
+hs.hotkey.bind(mash2, 'q', function() hs.window.focusedWindow():move(units.center, topScreen, true) end)
